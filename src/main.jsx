8a0e7e5a5bea8522c9b795fe0a5160d6b0949d31
Galvin <galvinnguyen123@gmail.com>
2026-04-27 14:35:27 +0700

    lesson 01: config layer provider auth0

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,18 @@
 // Author: TrungQuanDev | https://youtube.com/@trungquandev
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Auth0Provider } from '@auth0/auth0-react'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Auth0Provider
+    domain='dev-enohvc7egd1m41wp.us.auth0.com'
+    clientId='IfLk2S56b0VOcTUdJhReXQaNj4GCxCSE'
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+    useRefreshTokens={true}
+    cacheLocation='localstorage'
+  >
+    <App />
+  </Auth0Provider>
+)
